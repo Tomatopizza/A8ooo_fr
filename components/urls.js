14fe36de4aa082440fb5f0d8189a5ac0ed360fa6
Giho Bang <googong522@naver.com>
2023-06-23 12:15:57 +0900

    server

--- components/urls.js
+++ components/urls.js
@@ -1,3 +1,2 @@
-const back_base_url = "http://127.0.0.1:8000"
-
-const front_base_url = "http://127.0.0.1:5500"
+const back_base_url = "https://awe3omereview.link"
+const front_base_url = "https://cdn.awe3omereview.link"
